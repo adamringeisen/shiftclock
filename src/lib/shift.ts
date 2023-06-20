1031934b745a8b5fc542c11b0122ba998efacbfa
Adam Ringeisen <ringeisen@gmail.com>
2023-06-20 11:13:42 -0400

    Update shift.ts

--- src/lib/shift.ts
+++ src/lib/shift.ts
@@ -1,3 +1,3 @@
 export default function getShift(n: number): string {
-  if (n > 16) { return "Pre-Shift" } else if (n < -16 && n > 8) { return "Shift" } else return "Post-Shift"
+  if (n > 16) { return "Pre-Shift" } else if (n <= 16 && n > 8) { return "Shift" } else return "Post-Shift"
 }
